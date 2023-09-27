829e6214d8c3482e36bdeeead440f83cb2951a12
chino702 <chinox3702@gmail.com>
2023-09-27 15:34:20 -0700

    imported NavBar into my app.js file

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import Dashboard from "./Components/Dashboard";
-import AddAssetForm from "./Components/AddAssetForm";
-
+import Dashboard from "./Dashboard";
+import AddAssetForm from "./AddAssetForm";
+import NavBar from "./NavBar";
 
 function App() {
   return(
     <Router>
+      <NavBar/>
       <Switch>
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/add-asset" component={AddAssetForm} />
